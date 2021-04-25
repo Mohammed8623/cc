dade0401c828979c6a93cc3c9e4d39a33eeb9838
mohammed-source <naseermd340@gmail.com>
2021-04-25 10:58:27 -0500

    Initial Commit

---
+++ website/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+def home(request):
+	return render(request, 'home.html', {})
+
+def contact(request):
+	if request.method == "POST":
+		message_name = request.POST['message-name']
+		message_email =request.POST['message-email']
+		message = request.POST['message']
+		
+		send_mail(
+			message_name,
+			message,
+			message_email,
+			['drjones@gmail.com'],
+			fail_silently=False,
+			)
+
+
+
+
+
+
+
+
+		return render(request, 'contact.html', {'message_name': message_name})
+	else:
+
+		return render(request, 'contact.html', {})
+
+
+
+
+
+
+
+
+
+
+
+'''def about(request):
+	return render(request, 'about.html', {})
+
+def blog-details(request):
+	return render(request, 'blog-details.html', {})
+
+def pricing(request):
+	return render(request, 'pricing.html', {})
+
+def service(request):
+	return render(request, 'service.html', {})
+
+def blog(request):
+	return render(request, 'blog.html', {})
+'''
