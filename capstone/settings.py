17942efe3b154b108b87dc0d3191effa38ce2af0
Mohammed Naseer <mnaseer8623@gmail.com>
2021-04-26 01:26:11 -0500

    Initial Commit

--- capstone/settings.py
+++ capstone/settings.py
@@ -117,8 +117,8 @@ STATICFILES_DIRS = [
 '''
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT =  587
-EMAIL_HOST_USER = 'naseermd340@gmail.com'
-EMAIL_HOST_PASSWORD = 'purenaseer1'
+EMAIL_HOST_USER = 'mnaseercap@gmail.com'
+EMAIL_HOST_PASSWORD = 'pureNaseer@23'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 '''
